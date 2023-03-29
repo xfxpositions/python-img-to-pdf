133aba49a068ac9262521f875b54b49cb9eaca8e
xfxpositions <xfxpositions@gmail.com>
2023-03-29 03:16:34 +0300

    cli complete

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python image to pdf converter.
+## Usage
+download the binary from releases
+for cli, use `python ./run.py` or `cli.exe` for windows
+for gui, use `python ./run.py --gui` or `pdf_converter.exe` for windows
+
+## Installing from source.
+navigate to the directory containing the `requirements.txt` file and run the following command to install the dependencies:
+`pip install -r requirements.txt`
+---
+#### Yusuf Karaca, 29/03/2023
--- run.py
+++ run.py
@@ -1,6 +1,20 @@
 import os
 from PIL import Image, ImageOps, ImageChops
 from fpdf import FPDF
+import argparse
+
+# create an argument parser
+parser = argparse.ArgumentParser(description='Convert images to PDF')
+
+# add an optional argument for output file name
+parser.add_argument('-o', '--output', default='çıktı', help='output PDF file name (without extension) example: -o test')
+
+# parse the command line arguments
+args = parser.parse_args()
+
+# access the output file name
+output_file_name = args.output
+
 
 input_folder_name = input("girdiler icin klasor ismi giriniz:\n")
 output_folder_name = input("ciktilar icin klasor ismi giriniz:\n")
@@ -13,11 +27,6 @@ if not os.path.exists(input_folder_path):
 
 if not os.path.exists(output_folder_path):
     os.makedirs(output_folder_name)
-
-for filename in os.listdir(output_folder_path):
-    # Print the filename
-    print(filename)
-
 
 # Define a function to convert images to PDF
 def convert_images_to_pdf(image_folder_path, pdf_file_path):
@@ -32,20 +41,6 @@ def convert_images_to_pdf(image_folder_path, pdf_file_path):
         # Open the image file using Pillow
         image = Image.open(os.path.join(image_folder_path, image_file))
 
-
-        # def trim(im):
-        #     bg = Image.new(im.mode, im.size, im.getpixel((0,0)))
-        #     diff = ImageChops.difference(im, bg)
-        #     diff = ImageChops.add(diff, diff, 2.0, -100)
-        #     bbox = diff.getbbox()
-        #     if bbox:
-        #         return im.crop(bbox)
-        # # Crop the image to remove any whitespace
-        # trim(image)
-        
-        # cropped_image_path = os.path.join(input_folder_path, 'cropped', image_file)
-        # image.save(cropped_image_path)
-
         # Add a new page to the PDF
         pdf.add_page()
 
@@ -59,16 +54,18 @@ def convert_images_to_pdf(image_folder_path, pdf_file_path):
 i = 1
 dirs = os.listdir(output_folder_path)
 for file in dirs:
-    if file.startswith("cikti") and os.path.isfile(file):
+    if file.startswith(output_file_name):
         i += 1
 
-pdf_file_name = f'cikti{i}.pdf'
+pdf_file_name = f'{output_file_name}{i}.pdf'
 pdf_file_path = os.path.join(output_folder_path, pdf_file_name)
 
 # Convert all images in the input folder to a PDF file
 convert_images_to_pdf(input_folder_path, pdf_file_path)
 
 print(f'PDF file saved as {pdf_file_path}')
+
+# Cropping white places in pdf
 print("Cropping....")
 
 import subprocess
@@ -90,17 +87,19 @@ for page in reader.pages:
     writer.add_page(page)
 
 writer.add_metadata(reader.metadata)
-cropped_name = os.path.splitext(pdf_file_path)[0] + '_cropped.pdf'
-a = os.path.join(output_folder_path, cropped_name)
-final_name = os.path.splitext(pdf_file_path)[0] + '_final.pdf'
-final_path = os.path.join(output_folder_path, final_name)
-pdf_file_path = os.path.join(output_folder_path, cropped_name)
 
-print(os.path.join(output_folder_path, f'{pdf_file_name}'))
-print(os.path.join(output_folder_path, cropped_name))
-
+# Writing final
 with open(os.path.join(output_folder_path, f"{pdf_file_name}_final.pdf"), "wb") as fp:
     writer.write(fp)
+
+# Deleting cropped and first files
+cropped_name = os.path.splitext(pdf_file_path)[0] + '_cropped.pdf'
+final_name = os.path.splitext(pdf_file_path)[0] + '_final.pdf'
+try:
     os.remove(os.path.join(output_folder_path, f'{pdf_file_name}'))
     os.remove(cropped_name)
-print(f"DONE {final_name}")
+except OSError as e:
+    print(f'error deleting file: {e.filename} - {e.strerror}')
+finally:
+    print(f"DONE {final_name}")
+    
